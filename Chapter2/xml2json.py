adca88c44bd6e98add382b8fac5f92a43ba96f1f
Anna Novikova <anovik@yandex.ru>
2023-04-30 10:30:38 +0300

    xml2json.py changes

--- Chapter2/xml2json.py
+++ Chapter2/xml2json.py
@@ -1,10 +1,14 @@
 import xmltodict
 import json
+from zipfile import ZipFile
 
 with open("output.xml") as xml_file:         
     parsedDict = xmltodict.parse(xml_file.read(), attr_prefix='', cdata_key='text')    
     
-    json = json.dumps(parsedDict, indent=1)     
+json = json.dumps(parsedDict, indent=1)     
+
+with open("output.json", "w") as json_file:
+    json_file.write(json)       
     
-    with open("output.json", "w") as json_file:
-        json_file.write(json)       
+with ZipFile('output.zip', 'w') as zipFile:
+    zipFile.write('output.json')
