300164d26ffa019f98a0f7262f05f702d6fd55dc
Anna Novikova <anovik@yandex.ru>
2023-06-30 10:01:49 +0200

    Added experiment plots.

--- Chapter5/experiment.py
+++ Chapter5/experiment.py
@@ -12,6 +12,16 @@ current = [v/resistance + random.uniform(-0.02, 0.02) for v in voltage]
 
 print(current)
 
-plt.plot(voltage,current)
-plt.title("Current vs. voltage")
+plt.xlabel('Voltage', fontsize=14)
+plt.ylabel('Current', fontsize=14)
+plt.plot(voltage,current,'o')
+plt.title("Current vs. voltage", fontsize=20)
+plt.show()
+
+
+plt.xlabel('Voltage', fontsize=14)
+plt.ylabel('Current', fontsize=14)
+plt.plot(voltage,current,'o')
+plt.plot(voltage, [v/10 for v in voltage], 'r') 
+plt.title("Current vs. voltage", fontsize=20)
 plt.show()
